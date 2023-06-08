3abaef1a1191fd5d608180add53fc3c76c4351cd
Andrey Emelianov <621077@mail.ru>
2023-06-08 18:54:56 +0300

    реализовал отображение и скрытие корзины при клике на кнопку корзина

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,12 +1,21 @@
-import styles from "./CartButton.module.css";
+import { useDispatch } from 'react-redux';
+import { mainActions } from '../../store/main-slice';
 
-const CartButton = (props) => {
-  return (
-    <button className={styles.button}>
-      <span>Корзина</span>
-      <span className={styles.badge}>2</span>
-    </button>
-  );
+import styles from './CartButton.module.css';
+
+const CartButton = () => {
+	const dispatchFunction = useDispatch();
+
+	const cartVisibilityHandler = () => {
+		dispatchFunction(mainActions.toggleCartVisibility());
+	};
+
+	return (
+		<button className={styles.button} onClick={cartVisibilityHandler}>
+			<span>Корзина</span>
+			<span className={styles.badge}>2</span>
+		</button>
+	);
 };
 
 export default CartButton;
