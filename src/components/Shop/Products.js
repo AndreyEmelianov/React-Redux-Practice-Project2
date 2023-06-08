a1683927f47936638ca3da2aedb498ad4b9e3f98
Andrey Emelianov <621077@mail.ru>
2023-06-08 19:17:54 +0300

    реализовал экшен удаления товара и создал хардкодед список товаров

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,19 +1,44 @@
-import ProductItem from "./ProductItem";
-import styles from "./Products.module.css";
+import ProductItem from './ProductItem';
+import styles from './Products.module.css';
 
-const Products = (props) => {
-  return (
-    <section className={styles.products}>
-      <h2>В нашем магазине товары самого высокого качества</h2>
-      <ul>
-        <ProductItem
-          title="Супер-Товар"
-          price={7}
-          description="Благодаря своему высокому качеству, этот товар прослужит вам очень долго."
-        />
-      </ul>
-    </section>
-  );
+const DUMMY_ITEMS = [
+	{
+		id: '1',
+		price: 7,
+		title: 'Red Bull',
+		description: 'Благодаря своему высокому качеству, этот товар взбодрит вас на долго.',
+	},
+	{
+		id: '2',
+		price: 11,
+		title: 'Протеиновый Батончик',
+		description: 'Благодаря своему высокому качеству, этот товар насытит вас на долго.',
+	},
+	{
+		id: '3',
+		price: 9,
+		title: 'Витамин С',
+		description:
+			'Благодаря своему высокому качеству, этот товар восполнит нехватку витамина С.',
+	},
+];
+
+const Products = () => {
+	return (
+		<section className={styles.products}>
+			<h2>В нашем магазине товары самого высокого качества</h2>
+			<ul>
+				{DUMMY_ITEMS.map((item) => (
+					<ProductItem
+						key={item.id}
+						title={item.title}
+						price={item.price}
+						description={item.description}
+					/>
+				))}
+			</ul>
+		</section>
+	);
 };
 
 export default Products;
